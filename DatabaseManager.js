63efce2de8c1e9f496ec4cc0c93c945ad471f7cc
Dhruv - Laptop (Linux) <druthers1993@gmail.com>
2016-12-17 02:56:37 -0500

    Failed... Miserably.... DatabaseManager clas thing doesnt work. Learn to use classes in Node as currntly everything of another class is undefined

--- DatabaseManager.js
+++ DatabaseManager.js
@@ -5,17 +5,68 @@ var mongodb = require("mongodb"); //lets require/import the mongodb native drive
 var MongoClient = mongodb.MongoClient;
 
 // Connection URL. This is where your mongodb server is running.
-var dburl = "mongodb://localhost:27017/local";
+var dburl = "";
+var db;
 var collections = {};
+var isConnected = false;
+var dbMessages = ["DB_NOT_CONNECTED", "COLLECTION_NOT_FOUND", "COULD_NOT_INSERT"];
+
 
 function DBManager(dbUrl) {
-  // always initialize all instance properties
-  this.bar = bar;
-  this.baz = 'baz'; // default value
+  dburl = dbUrl;
 }
-// class methods
-Foo.prototype.fooBar = function() {
 
+
+// class methods
+DBManager.prototype.Connect = function(){
+  console.log(dburl);
+  MongoClient.connect(dburl, function(err, db) {
+    if(err){
+      console.log("\n\nError while connecting to database:");
+      console.log(err);
+      isConnected = false;
+      return dbMessages[0];
+    } else{
+      this.db = db;
+      isConnected = true;
+      return true;
+    }
+  });
 };
+
+
+DBManager.prototype.AddCollection = function(collectionName){
+  if(isConnected){
+    db.collection(collectionName, {strict:true}, function(err, collection){
+      if(err){
+        console.log("\n\nError while adding collection:");
+        console.log(err);
+        return dbMessages[1];
+      } else{
+        collections[collectionName] = collection;
+        return true;
+      }
+    });
+  } else{
+    return dbMessages[0];
+  }
+};
+
+DBManager.prototype.InsertRows = function(collectionName, data){
+  if(isConnected){
+    collections[collectionName].insert(data, function(err, result) {
+      if (err) {
+        console.log("\n\nError while inserting rows:");
+        console.log(err);
+        return dbMessages[2];
+      } else {
+        return true;
+      }
+    });
+  } else{
+    return dbMessages[0];
+  }
+};
+
 // export the class
 module.exports = DBManager;
--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var tmi = require("tmi.js");
-
-
+var DBManager = require("./DatabaseManager.js");
 var streamerName = "moonmoon_ow"; //""theonemanny;
 var ircOptions = {
   options: {
@@ -19,126 +18,101 @@ var userJoinBackLog = [];
 
 var ircClient = new tmi.client(ircOptions);
 
-
-
-
-var mongodb = require("mongodb"); //lets require/import the mongodb native drivers.
-
-//We need to work with "MongoClient" interface in order to connect to a mongodb server.
-var MongoClient = mongodb.MongoClient;
-
-// Connection URL. This is where your mongodb server is running.
-var dburl = "mongodb://localhost:27017/";
-
-
-
 var request = require("request");
-
 
 
 // FOR VIEWER COUNT
 var viewerCounts = [];
+var dburl = "mongodb://localhost:27017/local";
+var dbManager = new DBManager(dburl);
 
 
+var temp2 = dbManager.Connect();
+console.log("Did we connect to the databse???" + temp2);
+var temp = dbManager.AddCollection(streamerName+"_chat_counts");
+if(temp){
+  console.log(streamerName+"_chat_counts added to the collections");
+} else{
+  console.log("COULD NOT ADD COLLECTION. Reason:" + temp);
+}
+//dbManager.AddCollection(streamerName + "_user_logs");
+//dbManager.AddCollection(streamerName + "_viewer_counts");
 
-
-
-// Use connect method to connect to the Server
-MongoClient.connect(dburl, function(err, db) {
-  if (err) {
-    console.log("Unable to connect to the mongoDB server. Error:" + err);
-  } else {
-    //HURRAY!! We are connected. :)
-    console.log("Connection established to " + dburl);
-
-    // Get the documents collection
-    var chatCountsCollections = db.collection(streamerName + "_chat_counts");
-    var userLogsCollections = db.collection(streamerName + "_user_logs");
-    var viewerCountsCollections = db.collection(streamerName + "_viewer_counts");
-
-
-    // Connect the client to the server..
-    ircClient.connect();
-
-    ircClient.on("chat", function(channel, userstate, message, self) {
-      // Don't listen to my own messages..
-      if (self) {
-        return;
-      }
-      // Do your stuff.
-      //console.log(userstate['display-name']);
-      //console.log("USERSTATE==================>\n");
-      //console.log(userstate);
-      var unixTimeSec = Math.floor(new Date() / 1000);
-      var ircLog = {
-        unixTimeSec: unixTimeSec,
-        username: userstate["display-name"]
-      };
-      chatCountsCollections.insert(ircLog, function(err, result) {
-        if (err) {
-          console.log("\n\nERROR:\n");
-          console.log(err);
-        } else {
-          //console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', result.length, result);
-        }
-      });
-    });
-
-    ircClient.on("join", function(channel, username, self) {
-      if (userJoinBackLog.length > 0) {
-        chatCountsCollections.insert(userJoinBackLog, function(err, result) {
-          if (err) {
-            console.log(err);
-          } else {
-            //console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', result.length, result);
-          }
-        });
-      }
-    });
-
-    ircClient.on("error", function(message) {
-      console.log("ERROR:=====>" + message);
-    });
-
-
-    //push all the viewcount stuff into an array and then push it into the db after sometime
-    setTimeout(function() {
-      console.log("CAME HERE1");
-      var apiUrl = "https://api.twitch.tv/kraken/streams?channel="+streamerName+"&client_id=dcjnqfita9g9qzn55h1y2wnoanbzi8f";
-
-      request({
-        url: apiUrl,
-        json: true
-      }, function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-          try {
-            console.log("CAME HERE");
-            var viewerCount = body.streams[0].viewers;
-            var unixTimeSec = Math.floor(new Date() / 1000);
-            viewerCounts.push({
-              unixTimeSec: unixTimeSec,
-              viewerCount: viewerCount
-            });
-            console.log("ViewerCounts.lenght" + viewerCounts.length);
-            if (viewerCounts.length > 0) {
-              viewerCountsCollections.insert(viewerCounts, function(err, result) {
-                console.log("CAME HERE 3");
-                if (err) {
-                  console.log(err);
-                } else {
-                  console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', viewerCounts.length, result);
-                  viewerCounts = [];
-                }
-              });
-            }
-            console.log(viewerCount);
-          } catch (err) {
-
-          }
-        } else {
-          console.log("Something went wrong while trying to get the viewer count for " + streamerName);
-        }
-      }, 3000);
-    });
+// Connect the client to the server..
+ircClient.connect();
+ircClient.on("chat", function(channel, userstate, message, self) {
+  // Don't listen to my own messages..
+  if (self) {
+    return;
   }
+  // Do your stuff.
+  //console.log(userstate['display-name']);
+  //console.log("USERSTATE==================>\n");
+  //console.log(userstate);
+  var unixTimeSec = Math.floor(new Date() / 1000);
+  var ircLog = {
+    unixTimeSec: unixTimeSec,
+    username: userstate["display-name"]
+  };
+  dbManager.InsertRows(streamerName+"_chat_counts", ircLog);
 });
+
+
+
+//Pretty much never works. TwitchAPI said its disable for big channels and also that they're grouped and sent every 30 secs or
+// so we'll need a lot of logic to handle that too.
+ircClient.on("join", function(channel, username, self) {
+  /*if (userJoinBackLog.length > 0) {
+    chatCountsCollections.insert(userJoinBackLog, function(err, result) {
+      if (err) {
+        console.log(err);
+      } else {
+        //console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', result.length, result);
+      }
+    });
+  }*/
+});
+
+
+ircClient.on("error", function(message) {
+  console.log("ERROR:=====>" + message);
+});
+
+/*
+//push all the viewcount stuff into an array and then push it into the db after sometime
+setTimeout(function() {
+  console.log("CAME HERE1");
+  var apiUrl = "https://api.twitch.tv/kraken/streams?channel=" + streamerName + "&client_id=dcjnqfita9g9qzn55h1y2wnoanbzi8f";
+  request({
+    url: apiUrl,
+    json: true
+  }, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      try {
+        console.log("CAME HERE");
+        var viewerCount = body.streams[0].viewers;
+        var unixTimeSec = Math.floor(new Date() / 1000);
+        viewerCounts.push({
+          unixTimeSec: unixTimeSec,
+          viewerCount: viewerCount
+        });
+        console.log("ViewerCounts.lenght" + viewerCounts.length);
+        if (viewerCounts.length > 0) {
+          viewerCountsCollections.insert(viewerCounts, function(err, result) {
+            console.log("CAME HERE 3");
+            if (err) {
+              console.log(err);
+            } else {
+              console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', viewerCounts.length, result);
+              viewerCounts = [];
+            }
+          });
+        }
+        console.log(viewerCount);
+      } catch (err) {}
+    } else {
+      console.log("Something went wrong while trying to get the viewer count for " + streamerName);
+    }
+  });
+}, 3000);
+*/
