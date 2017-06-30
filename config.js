bcbdeae15f2583a29da6c5aa1cac7a6f654d086f
Dhruv - PC <pateldhruv1993@gmail.com>
2017-06-30 03:04:36 -0400

    Added fake config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+var config = {
+    dbUrl: "<YOUR DB URL>",
+
+
+    twitch: {
+        username: "<YOUR TWITCH USERNAME>",
+        password: "<YOUR TWICH API OAUTH PASSWORD>",
+        clientId: "<YOUR TWICH API CLIENT ID>",
+    },
+
+    streamerList: ["ARRAY", "OF", "STREAMER", "TWITCH", "USERNAMES"],
+
+};
+
+
+module.exports = config;
